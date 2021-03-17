ad2122d6827ea9d8233a677ee625d69426509b3e
Devaditya558 <76488412+Devaditya558@users.noreply.github.com>
2021-03-17 14:59:07 +0530

    sumnum

---
+++ 4_sumnum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int n1,n2,s ;
+	printf("enter the first number\n");
+	scanf("%d",&n1);
+	printf("enter the second function\n");
+	scanf("%d",&n2);
+	printf("%d sum",n1+n2);
+	return 0;
+}
