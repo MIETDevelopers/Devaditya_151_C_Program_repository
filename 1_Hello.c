886b8ec49259bdbdd1ad53a2a00e0890305becf1
Devaditya558 <76488412+Devaditya558@users.noreply.github.com>
2021-03-18 17:49:58 +0530

    modified

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,5 +1,10 @@
-#include<stdio.h>
-int main(){
-	printf("hello world\n");
-	return 0;
-}
+//Author name: Devaditya sharma 
+//creation date: 17-03-2021
+// purpose: To Print Hello World program
+#include<stdio.h> //Preprocessor directive to include input output function header file
+//start of the main body function 
+int main(){ 
+	printf("Hello World");//Printf function calling to print Hellow World
+	return 0;//Return statement
+	} 
+//End of the main body function
